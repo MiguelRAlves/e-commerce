c5939ad05624dbc05463b68a58254f46bbbfb18c
Miguel Alves <pokassbp@gmail.com>
2025-05-03 14:57:13 -0300

    feat: add getProducts endpoint

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import productRoutes from './routes/productRoutes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', productRoutes)
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
