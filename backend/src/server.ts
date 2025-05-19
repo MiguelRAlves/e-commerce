f1e37ecb5d1da9501b20ad132fa074ab68595503
Miguel Alves <pokassbp@gmail.com>
2025-05-19 10:43:39 -0300

    feat: add success and cancel pages after payment session

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import path from 'path';
 import { limiter } from './controllers/authController';
 
 
@@ -22,6 +23,7 @@ app.use(helmet());
 app.use(limiter);
 app.post('/webhook', bodyParser.raw({ type: 'application/json' }), stripeWebhookController);
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use('/api/auth', authRoutes);
 
@@ -29,6 +31,13 @@ app.use('/api', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes)
 app.use('/api/payment', paymentRoutes)
+
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "success.html"));
+});
+app.get("/cancel", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "cancel.html"));
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
