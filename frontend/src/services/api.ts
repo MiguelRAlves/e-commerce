a9854235f9d8460a99add92f9ba08f255f194420
Miguel Alves <pokassbp@gmail.com>
2025-06-20 17:44:19 -0300

    adjusts for frontend deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://e-commerce-2fes.onrender.com/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
